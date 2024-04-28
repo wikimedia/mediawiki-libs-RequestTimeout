d557482eec47fbe20a31d479b13e23165ebbd7e8
Reedy <reedy@wikimedia.org>
2024-04-28 14:55:17 +0100

    .phan/config.php: Minor cleanup
    Change-Id: I5f47867d82453265be4e341179991f077819edf6

--- .phan/config.php
+++ .phan/config.php
@@ -6,16 +6,12 @@ $cfg['autoload_internal_extension_signatures'] = [
 	'excimer' => '.phan/internal_stubs/excimer.php',
 ];
 
-$cfg['directory_list'] = [
-	'src',
-	'vendor',
-	'tests'
-];
-$cfg['exclude_analysis_directory_list'][] = 'vendor';
+$cfg['directory_list'][] = 'tests';
 $cfg['suppress_issue_types'] = [
 	// It's a library, methods don't have to be called
 	'PhanUnreferencedPublicMethod',
 	// It means internal to the library
 	'PhanAccessMethodInternal',
 ];
+
 return $cfg;
